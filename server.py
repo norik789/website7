ef359acde65205b06f61238a1118f7fa698f7192
Your Name <norikmkrt@mail.ru>
2020-06-26 00:54:31 +0300

    Деплой1

--- server.py
+++ server.py
@@ -1,1 +1,9 @@
-import Django_504
+import django
+from  django.http import  HttpResponse
+
+
+def index (request):
+    return HttpResponse('Hello world')
+
+def status (request):
+    return HttpResponse('Ok world')
